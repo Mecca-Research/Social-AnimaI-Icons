dc39f2a75a66a05159a0c75c41e026d4786cf3f5
Tariq Mohammed <thehealthfreaktv@gmail.com>
2025-08-09 14:32:41 -0400

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SocialAnimalsRPG from './SocialAnimalsRPG'
+
+export default function App() {
+  return (
+    <div className="w-screen h-screen">
+      <SocialAnimalsRPG />
+    </div>
+  )
+}
